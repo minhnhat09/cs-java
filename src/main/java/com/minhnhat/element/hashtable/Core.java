b7b021b844507673c8d663ac06cb7fe7d46e4f69
NGUYEN Nhat Minh <nhatminh1809@gmail.com>
2019-10-20 10:29:21 +0200

    add examples hashtable

--- src/main/java/com/minhnhat/element/hashtable/Core.java
+++ src/main/java/com/minhnhat/element/hashtable/Core.java
@@ -3,6 +3,7 @@ package com.minhnhat.element.hashtable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -28,4 +29,30 @@ public class Core {
     }
     return results;
   }
+
+  public static class ContactList {
+    public List<String> names;
+
+    ContactList(List<String> names) {
+      this.names = names;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == null || !(obj instanceof ContactList)) {
+        return false;
+      }
+      return this == obj ? true : new HashSet<>(names).equals(new HashSet<>(((ContactList) obj).names));
+    }
+
+    @Override
+    public int hashCode() {
+      return new HashSet<>(names).hashCode();
+    }
+  }
+
+  public static List<ContactList> mergeContactLists(List<ContactList> contacts) {
+    return new ArrayList<>(new HashSet<>(contacts));
+  }
+
 }
