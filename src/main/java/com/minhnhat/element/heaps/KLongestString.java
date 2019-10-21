ff4b912a06c8c8e14e1e662ce7f17a5b830d5b8e
NGUYEN Nhat Minh <naht-minh.nguyen_externe@groupe-sma.fr>
2019-10-21 14:15:28 +0200

    add merge sorted file

---
+++ src/main/java/com/minhnhat/element/heaps/KLongestString.java
@@ -0,0 +1,24 @@
+package com.minhnhat.element.heaps;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KLongestString {
+  public static List<String> topK(int k, Iterator<String> iter) {
+    PriorityQueue<String> minHeap = new PriorityQueue<>(k, new Comparator<String>() {
+      public int compare(String s1, String s2) {
+        return Integer.compare(s1.length(), s2.length());
+      }
+    });
+    while (iter.hasNext()) {
+      minHeap.add(iter.next());
+      if (minHeap.size() > k) {
+        minHeap.poll();
+      }
+    }
+    return new ArrayList<>(minHeap);
+  }
+}
---
+++ src/main/java/com/minhnhat/element/heaps/MergeSortedFile111.java
@@ -0,0 +1,46 @@
+package com.minhnhat.element.heaps;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class MergeSortedFile {
+  public static class ArrayEntry {
+    public Integer value;
+    public Integer arrayId;
+
+    public ArrayEntry(Integer value, Integer arrayId) {
+      this.value = value;
+      this.arrayId = arrayId;
+    }
+  }
+
+  public static List<Integer> mergeSortedArrays(List<List<Integer>> sortedArrays) {
+    List<Iterator<Integer>> iters = new ArrayList<>(sortedArrays.size());
+    for (List<Integer> array : sortedArrays) {
+      iters.add(array.iterator());
+    }
+    PriorityQueue<ArrayEntry> minHeap = new PriorityQueue<>((int) sortedArrays.size(), new Comparator<ArrayEntry>() {
+      @Override
+      public int compare(ArrayEntry o1, ArrayEntry o2) {
+        return Integer.compare(o1.value, o2.value);
+      }
+    });
+    for (int i = 0; i < iters.size(); i++) {
+      if (iters.get(i).hasNext()) {
+        minHeap.add(new ArrayEntry(iters.get(i).next(), i));
+      }
+    }
+    List<Integer> result = new ArrayList<>();
+    while (!minHeap.isEmpty()) {
+      ArrayEntry headEntry = minHeap.poll();
+      result.add(headEntry.value);
+      if (iters.get(headEntry.arrayId).hasNext()) {
+        minHeap.add(new ArrayEntry(iters.get(headEntry.arrayId).next(), headEntry.arrayId));
+      }
+    }
+    return result;
+  }
+}
